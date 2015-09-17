b9c70ef45ab3dcc1582a3e1b97ee93a8c7abf97e
gdsanders <gd@digitalequity.com>
2015-09-17 17:47:43 -0500

    initWithData

--- AGPSpaceObject.h
+++ AGPSpaceObject.h
@@ -7,7 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface AGPSpaceObject : NSObject
+
+- (id)initWithData: (NSDictionary *)data andImage:(UIImage *)image;
 
 @end
