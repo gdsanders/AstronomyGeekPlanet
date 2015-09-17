e6702e23ec64543b1050e47ac83951d57afb996b
gdsanders <gd@digitalequity.com>
2015-09-17 17:07:24 -0500

    AGPSpaceObject

---
+++ AGPSpaceObject.h
@@ -0,0 +1,13 @@
+//
+//  AGPSpaceObject.h
+//  AstronomyGeekPlanet
+//
+//  Created by G.D. Sanders on 9/17/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGPSpaceObject : NSObject
+
+@end
---
+++ AGPSpaceObject.m
@@ -0,0 +1,13 @@
+//
+//  AGPSpaceObject.m
+//  AstronomyGeekPlanet
+//
+//  Created by G.D. Sanders on 9/17/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import "AGPSpaceObject.h"
+
+@implementation AGPSpaceObject
+
+@end
