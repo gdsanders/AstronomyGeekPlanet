d4444b6961c76de760468e419a0c902d975ba2fc
gdsanders <gd@digitalequity.com>
2015-09-22 14:21:45 -0500

    SpaceImageViewController added

---
+++ SpaceImageViewController.h
@@ -0,0 +1,16 @@
+//
+//  SpaceImageViewController.h
+//  AstronomyGeekPlanet
+//
+//  Created by G.D. Sanders on 9/22/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpaceImageViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+
+@end
